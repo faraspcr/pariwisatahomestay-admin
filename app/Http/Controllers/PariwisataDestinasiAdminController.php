600d47d239c54f3926b6bf98b02240343a00688d
faras <faras24si@mahasiswa.pcr.ac.id>
2025-10-01 20:10:10 +0700

    perubahan benar

--- app/Http/Controllers/PariwisataDestinasiAdminController.php
+++ app/Http/Controllers/PariwisataDestinasiAdminController.php
@@ -11,43 +11,15 @@ class PariwisataDestinasiAdminController extends Controller
      */
     public function index()
     {
+        return view('pariwisatadestinasi_admin');
         $destinasi = [
-            [
-                'destinasi_id' => 1,
-                'nama' => 'Pantai Indah',
-                'deskripsi' => 'Pantai dengan pemandangan sunset yang menakjubkan dan pasir putih yang bersih',
-                'alamat' => 'Jl. Pantai Indah No. 1',
-                'rt' => '01',
-                'rw' => '02',
-                'jam_buka' => '06:00 - 18:00',
-                'tiket' => 15000,
-                'kontak' => '081234567890'
-            ],
-            [
-                'destinasi_id' => 2,
-                'nama' => 'Air Terjun Segar',
-                'deskripsi' => 'Air terjun dengan air yang jernih dan sejuk, cocok untuk refreshing',
-                'alamat' => 'Jl. Air Terjun No. 5',
-                'rt' => '03',
-                'rw' => '01',
-                'jam_buka' => '07:00 - 17:00',
-                'tiket' => 20000,
-                'kontak' => '081234567891'
-            ],
-            [
-                'destinasi_id' => 3,
-                'nama' => 'Bukit Asri',
-                'deskripsi' => 'Bukit dengan pemandangan desa yang luas dan udara segar',
-                'alamat' => 'Jl. Bukit Asri No. 10',
-                'rt' => '02',
-                'rw' => '04',
-                'jam_buka' => '05:00 - 19:00',
-                'tiket' => 10000,
-                'kontak' => '081234567892'
-            ],
+            ['nama' => 'Pantai', 'alamat' => 'RT 01 RW 02', 'tiket' => 15000],
+            ['nama' => 'Air Terjun Segar', 'alamat' => 'RT 03 RW 01', 'tiket' => 20000],
+            ['nama' => 'Bukit Asri', 'alamat' => 'RT 02 RW 04', 'tiket' => 12000],
         ];
 
-    return view('pariwisatadestinasi_admin', compact('destinasi'));
+        // ✨ Passing data ke view
+        return view('pariwisatadestinasi_admin', compact('destinasi'));
     }
 
 
