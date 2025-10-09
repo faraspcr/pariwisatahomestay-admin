06481c5ba758875d19e0a16339c17865b09d444d
faraspcr <faras24si@mahasiswa.pcr.ac.id>
2025-10-09 13:49:59 +0700

    Perubahan pada destinasi2

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,10 @@ Route::get('/', function () {
 });
 
 // Route untuk auth
+// "Route /auth GET → menampilkan halaman login"
 Route::get('/auth', [AuthController::class, 'index'])->name('auth.index');
+
+//"Route /auth/login POST → memproses form login"
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout'); // ✅ TAMBAHKAN ROUTE LOGOUT
 
