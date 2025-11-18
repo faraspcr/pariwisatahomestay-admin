00babeaa2725d717a46954e7b3489884d570cb2a
faraspcr <faras24si@mahasiswa.pcr.ac.id>
2025-11-18 22:45:05 +0700

    relasi dua tabel CRUD

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,41 +1,65 @@
-{{-- ====================== START SIDEBAR ====================== --}}
-<!-- {{-- start sidebar --}} -->
-<!-- partial:partials/_sidebar.html -->
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-        <li class="nav-item nav-category">Utama</li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}" data-no-spa>
-                <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
-                <span class="menu-title">Dashboard</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('user.index') }}" data-no-spa>
-                <span class="icon-bg"><i class="mdi mdi-account menu-icon"></i></span>
-                <span class="menu-title">User</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('warga.index') }}" data-no-spa>
-                <span class="icon-bg"><i class="mdi mdi-account-multiple menu-icon"></i></span>
-                <span class="menu-title">Data Warga</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('destinasiwisata.index') }}" data-no-spa>
-                <span class="icon-bg"><i class="mdi mdi-map-marker menu-icon"></i></span>
-                <span class="menu-title">Destinasi Wisata</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('ulasan_wisata.index') }}" data-no-spa>
-                <span class="icon-bg"><i class="mdi mdi-account-group menu-icon"></i></span>
-                <span class="menu-title">Ulasan Wisata</span>
-            </a>
-        </li>
-    </ul>
-</nav>
-<!-- partial -->
-<!-- {{-- end sidebar --}} -->
-{{-- ====================== END SIDEBAR ====================== --}}
+ <!-- ==================== START SIDEBAR ==================== -->
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item nav-category">Menu Utama</li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard') }}">
+                            <span class="icon-bg"><i class="mdi mdi-view-dashboard menu-icon"></i></span>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item nav-category">Fitur Utama</li>
+
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle" data-toggle="collapse" href="#fiturUtama" aria-expanded="false" aria-controls="fiturUtama">
+                            <span class="icon-bg"><i class="mdi mdi-apps menu-icon"></i></span>
+                            <span class="menu-title">Fitur Utama</span>
+                        </a>
+                        <div class="collapse" id="fiturUtama">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('destinasiwisata.index') }}">
+                                        <i class="mdi mdi-map-marker menu-icon"></i>
+                                        Destinasi Wisata
+                                    </a>
+                                </li>
+                               <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('ulasan_wisata.index') }}">
+                                        <i class="mdi mdi-account-group menu-icon"></i>
+                                       Ulasan Wisata
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li class="nav-item nav-category">Master Data</li>
+
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle" data-toggle="collapse" href="#masterData" aria-expanded="false" aria-controls="masterData">
+                            <span class="icon-bg"><i class="mdi mdi-database menu-icon"></i></span>
+                            <span class="menu-title">Master Data</span>
+                        </a>
+                        <div class="collapse" id="masterData">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('user.index') }}">
+                                        <i class="mdi mdi-account-multiple menu-icon"></i>
+                                        Manajemen User
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('warga.index') }}">
+                                        <i class="mdi mdi-account-group menu-icon"></i>
+                                        Data Warga
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                </ul>
+            </nav>
+            <!-- ==================== END SIDEBAR ==================== -->
